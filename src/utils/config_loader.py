533c89340195acc565ce5dc6047cd6e9bcc1d548
Makenion <esteban.tamayo.chamorro@gmail.com>
2026-01-05 17:57:10 -0300

    feat: implementar filtrado por palabras clave para el stack tecnológico local

--- src/utils/config_loader.py
+++ src/utils/config_loader.py
@@ -6,7 +6,9 @@ load_dotenv()
 def get_config():
     config = {
         "NVD_API_KEY": os.getenv("NVD_API_KEY"),
-        "DISCORD_WEBHOOK": os.getenv("DISCORD_WEBHOOK_URL")
+        "DISCORD_WEBHOOK": os.getenv("DISCORD_WEBHOOK_URL"),
+        "MIN_SCORE": float(os.getenv("MIN_SCORE", 0.0)),
+        "KEYWORDS": os.getenv("INTEREST_KEYWORDS", "").lower().split(",")
     }
 
     if not config["NVD_API_KEY"] or not config["DISCORD_WEBHOOK"]:
